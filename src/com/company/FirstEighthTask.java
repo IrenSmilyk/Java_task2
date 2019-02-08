6622e8ca2d156794b087a556de0c509b42a5f230
IrenSmilyk <smilykirina@gmail.com>
2019-02-08 17:25:56 +0200

    Add new methods

--- src/com/company/FirstEighthTask.java
+++ src/com/company/FirstEighthTask.java
@@ -6,17 +6,18 @@ class FirstEighthTask {
     private String[] array;
     private String minStr = null;
     private String maxStr = null;
+    private Scanner scan = new Scanner(System.in);
+    String input;
 
 
-   /*-- Ввести n чисел с консоли.----------------------------------------------------------
-        1. Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.*/
-
-    //-----------Первый вариант------------------------------------------------------------
+    /*-- Ввести n чисел с консоли.----------------------------------------------------------
+         1. Найти самое короткое и самое длинное число. Вывести найденные числа и их длину.*/
+//-----------Первый вариант------------------------------------------------------------
     void showShortLongNumber() {
         int minCol = 1000;
         int maxCol = 0;
         System.out.print("Введите числа: ");
-        String input = new Scanner(System.in).nextLine();
+        input = scan.nextLine();
         array = input.split("\\s+");
         for (String i : array) {
             if (i.length() < minCol) {
@@ -37,7 +38,7 @@ class FirstEighthTask {
         }
     }
 
-    //-----------Второй вариант---------------------------------------------
+//-----------Второй вариант-------------------------------------------------------
    /* void showShortLongNumber() {
         long number;
         int counter = 1;
@@ -79,13 +80,44 @@ class FirstEighthTask {
             System.out.printf("\nМаксимальное число: %d состоит из %d цифр.", maxNum, maxCol);
         }
     }*/
+
     //---2. Упорядочить и вывести числа в порядке возрастания (убывания) значений их длины.--------
+    void sortNumbers() {
+        System.out.print("Числа в порядке возрастания значений их длины: ");
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].length() > array[j].length()) {
+                    String tmp = array[j];
+                    array[j] = array[i];
+                    array[i] = tmp;
+                }
+            }
+        }
+        for (String element : array) {
+            System.out.print(element + ", ");
+        }
+
+        System.out.print("\nЧисла в порядке убывания значений их длины: ");
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].length() < array[j].length()) {
+                    String tmp = array[j];
+                    array[j] = array[i];
+                    array[i] = tmp;
+                }
+            }
+        }
+        for (String element : array) {
+            System.out.print(element + ", ");
+        }
+    }
+
     //---3. Вывести на консоль те числа, длина которых меньше (больше) средней, а также длину.---
     void showAverageLength() {
         int sum = 0;
         int average;
         int counter = 0;
-        System.out.print("Длина введенных чисел: ");
+        System.out.print("\nДлина введенных чисел: ");
         for (String i : array) {
             System.out.print(i.length() + "  ");
             sum += i.length();
@@ -110,10 +142,28 @@ class FirstEighthTask {
         if (counter == 0) System.out.println("Таких чисел нет!");
 
     }
+
     //  4. Найти число, в котором число различных цифр минимально. Если таких чисел несколько, найти первое из них.
 //5. Найти количество чисел, содержащих только четные цифры, а среди них количество чисел с равным числом четных и нечетных цифр.
 //6. Найти число, цифры в котором идут в строгом порядке возрастания. Если таких чисел несколько, найти первое из них.
 //7. Найти число, состоящее только из различных цифр. Если таких чисел несколько, найти первое из них.
-//8. Вывести числа от 1 до k в виде матрицы N x N слева направо и сверху вниз.
+    void differentNumbers() {
+
+
+
+
+    }
+
+    //8. Вывести числа от 1 до k в виде матрицы N x N слева направо и сверху вниз.
+    void showArray() {
+        System.out.print("Введите размерность матрицы NxN: ");
+        int n = scan.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(i * n + j + 1 + " ");
+            }
+            System.out.println();
+        }
+    }
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        /*FirstEighthTask firstTask = new FirstEighthTask();
+        FirstEighthTask firstTask = new FirstEighthTask();
         firstTask.showShortLongNumber();
-        firstTask.showAverageLength();*/
-        NinthTask ninthTask =new NinthTask();
+        firstTask.sortNumbers();
+        firstTask.showAverageLength();
+        firstTask.showArray();
+        firstTask.differentNumbers();
+      /*  NinthTask ninthTask =new NinthTask();
         ninthTask.sedArray();
-        ninthTask.showArray();
+        ninthTask.showArray();*/
 
     }
 }
