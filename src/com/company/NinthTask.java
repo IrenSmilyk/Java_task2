76bfa58e9db81accef7f078406edeac478cbf130
IrenSmilyk <smilykirina@gmail.com>
2019-02-12 18:33:45 +0200

    Add task 4-6

--- src/com/company/NinthTask.java
+++ src/com/company/NinthTask.java
@@ -6,47 +6,42 @@ import java.util.Scanner;
 
 class NinthTask {
 
-
     private int[][] array;
     private int length;
 
+    /*--9.Ввести с консоли n-размерность матрицы a [n] [n]. Задать значения элементов матрицы в интервале значений---------
+    ---от -n до n с помощью датчика случайных чисел.---------------------------------------------------------------------*/
     void sedArray() {
-
         System.out.print("Введите размерность матрицы: ");
         Scanner input = new Scanner(System.in);
         length = input.nextInt();
         array = new int[length][length];
         Random ran = new Random();
-        Scanner low = new Scanner(System.in);
-        System.out.print("Введите нижнее значение интервала случайных чисел: ");
-        int low1 = low.nextInt();
-        System.out.print("Введите верхнее значение интервала случайных чисел: ");
-        int height = low.nextInt();
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                array[i][j] = ran.nextInt(height - low1) + 1;
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array.length; col++) {
+                array[row][col] = ran.nextInt(length + length) - length;
             }
         }
     }
 
     void showArray1() {
-        System.out.printf("Матрица %dx%d:\n", length, length);
-        for (int[] i : array) {
-            for (int j : i) {
-                System.out.print(j + "\t");
+        for (int[] row : array) {
+            for (int col : row) {
+                System.out.print(col + "\t");
             }
             System.out.print("\n");
         }
     }
 
+    //----9.1. Упорядочить строки (столбцы) матрицы в порядке возрастания значений.----------------------------------------
     void sortRowColumn() {
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                for (int k = j + 1; k < length; k++) {
-                    if (array[i][j] > array[i][k]) {
-                        int temp = array[i][k];
-                        array[i][k] = array[i][j];
-                        array[i][j] = temp;
+        for (int row = 0; row < length; row++) {
+            for (int col = 0; col < length; col++) {
+                for (int k = col + 1; k < length; k++) {
+                    if (array[row][col] > array[row][k]) {
+                        int temp = array[row][k];
+                        array[row][k] = array[row][col];
+                        array[row][col] = temp;
                     }
                 }
             }
@@ -54,23 +49,61 @@ class NinthTask {
         System.out.println("Матрица с упорядоченными строками в порядке возрастания значений: ");
         showArray1();
 
-        /*for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j < length; j++) {
-                for (int l = 0; l <length ; l++) {
-                    if (array[i][l] < array[j][l]) {
-                        for (int k = 0; k < length; k++) {
-                            int temp = array[i][k];
-                            array[i][k] = array[j][k];
-                            array[j][k] = temp;
-                        }
-
+        for (int row = 0; row < length; row++) {
+            for (int col = 0; col < length; col++) {
+                for (int j = row + 1; j < length; j++) {
+                    if (array[row][col] > array[j][col]) {
+                        int temp = array[j][col];
+                        array[j][col] = array[row][col];
+                        array[row][col] = temp;
                     }
                 }
             }
         }
 
         System.out.println("Матрица с упорядоченными столбцами в порядке возрастания значений: ");
-        showArray1();*/
+        showArray1();
+    }
+//---9.2. Выполнить циклический сдвиг заданной матрицы на k позиций вправо (влево, вверх, вниз).-----------------------
+//---9.3. Найти и вывести наибольшее число возрастающих (убывающих) элементов матрицы, идущих подряд.------------------
+//---9.4. Найти сумму элементов матрицы, расположенных между первым и вторым положительными элементами каждой строки.--
+
+//---9.5. Повернуть матрицу на 90 (180, 270) градусов против часовой стрелки.------------------------------------------
+//---9.6 Построить матрицу, вычитая из элементов каждой строки матрицы ее среднее арифметическое.----------------------
+    void minusAverage() {
+        int sum = 0;
+        int average;
+        int l = 0;
+        for (int row = 0; row < array.length; row++) {
+            for (int col = 0; col < array.length; col++) {
+                sum = sum + array[row][col];
+                l++;
+            }
+            average = sum / l;
+            sum = 0;
+            l = 0;
+            for (int col1 = 0; col1 < array.length; col1++) {
+                array[row][col1] = array[row][col1] - average;
+            }
+        }
+        System.out.println("Матрица, в кторой из элементов каждой строки вычли ее среднее арифметическое: ");
+        showArray1();
+    }
+//---9.7. Уплотнить матрицу, удаляя из нее строки и столбцы, заполненные нулями.----------------------------------------
+//---9.8. Преобразовать строки матрицы таким образом, чтобы элементы, равные нулю, располагались после всех остальных.--
+    void moveZero() {
+        for (int row = 0; row < array.length; row++) {
+            int n = 0;
+            int[] c = new int[array.length];
+            for (int col = 0; col < array.length; col++) {
+                if (array[row][col] != 0) {
+                    c[n++] = array[row][col];
+                }
+            }
+            array[row] = Arrays.copyOf(c, c.length);
+        }
+        System.out.println("Матрица в которой элементы, равные нулю, располагаются после всех остальных: ");
+        showArray1();
     }
 }
 
